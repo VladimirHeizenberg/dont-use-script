f7d26c569a5790e5468386a182a1ba14ac1c29d0
Vladimir <volodiakalganov@gmail.com>
2025-10-14 21:51:52 +0300

    test: more priority checks

--- tests/expressions/arithmetic_expressions_test.cpp
+++ tests/expressions/arithmetic_expressions_test.cpp
@@ -53,6 +53,17 @@ INSTANTIATE_TEST_SUITE_P(
         ExpressionCase{"-2^3", "-8"},
         ExpressionCase{"-2^4", "-16"},
         ExpressionCase{"(-2)^4", "16"},
+        ExpressionCase{"(2 + 3) * (4 + 1)", "25"},
+        ExpressionCase{"2 * (3 + 4 * (5 + 6))", "94"},
+        ExpressionCase{"(2 + 3 * (4 + 1)) ^ 2", "289"},
+        ExpressionCase{"(2 ^ 3) ^ 2", "64"},
+        ExpressionCase{"2 ^ (3 ^ 2)", "512"},
+        ExpressionCase{"-(-2 ^ 3)", "8"},
+        ExpressionCase{"-(2 ^ (1 + 2))", "-8"},
+        ExpressionCase{"(-2) ^ (1 + 2)", "-8"},
+        ExpressionCase{"20 / 5 / 2", "2"},
+        ExpressionCase{"20 / 2 / 5", "2"},
+
 
         // UnaryPlusMinus
         ExpressionCase{"-3 + 5", "2"},
@@ -89,6 +100,29 @@ INSTANTIATE_TEST_SUITE_P(
         ExpressionCase{"(1 < 2) and (2 < 3) and (3 < 4)", "true"},
         ExpressionCase{"(1 < 2) and (2 > 3) or (3 < 4)", "true"},
         ExpressionCase{"not (1 < 2 and 3 > 4)", "true"},
-        ExpressionCase{"not (1 < 2) and 3 > 4", "false"}
+        ExpressionCase{"not (1 < 2) and 3 > 4", "false"},
+        ExpressionCase{"(1 + 2) == 3 and (2 * 2) == 4", "true"},
+        ExpressionCase{"(1 + 2) == 4 or (2 * 2) == 4", "true"},
+        ExpressionCase{"(2 ^ 3) == 8 and not (3 ^ 2 == 8)", "true"},
+        ExpressionCase{"(5 / 2) == 2.5", "true"},
+        ExpressionCase{"(5 / 2) == 2", "false"},
+        ExpressionCase{"(5 % 2) == 1", "true"},
+        ExpressionCase{"(2 ^ 3) > (3 ^ 2)", "false"},
+
+
+        // floating
+        ExpressionCase{"1.5 + 2.3", "3.8"},
+        ExpressionCase{"2.0 * 0.5", "1"},
+        ExpressionCase{"5 / 2", "2.5"},
+        ExpressionCase{"5.0 / 2.0", "2.5"},
+        ExpressionCase{"1.2e3 + 3", "1203"},
+        ExpressionCase{"1.23e-2 * 100", "1.23"},
+        ExpressionCase{"-1.5e2", "-150"},
+        ExpressionCase{"2.5 ^ 2", "6.25"},
+        ExpressionCase{"2 + 3.5", "5.5"},
+        ExpressionCase{"3.5 - 1", "2.5"},
+        ExpressionCase{"2 * 0.5", "1"},
+        ExpressionCase{"4 / 2.0", "2"},
+        ExpressionCase{"2.0 ^ 3", "8"}
     )
 );
